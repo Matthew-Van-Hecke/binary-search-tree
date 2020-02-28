7010632cde2241a723d85f5c359c7660150b1617
Matthew VH <vanheckematt@gmail.com>
2020-02-28 14:39:23 -0600

    Created classes for BinaryTree and Node as well as a separate project for unit tests. Also wrote two unit tests for Add method which has yet to be built. Next goal: Lay out member variables for Node and BinaryTree classes.

---
+++ binarySearchTree/binarySearchTree_Test/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace binarySearchTree_Test
+{
+    [TestClass]
+    public class Binary_Tree_Test
+    {
+        [TestMethod]
+        public void Add_OneIntNode_RootInNotNull()
+        {
+            //Arrange
+            BinaryTree<Node<int>> tree = new BinaryTree<Node<int>>();
+            Node<int> node = new Node<int>(12);
+            //Act
+            tree.Add(node);
+            //Assert
+            Assert.IsNotNull(tree.root);
+        }
+        [TestMethod]
+        public void Add_OneIntNode_RootHasCorrectValue()
+        {
+            //Arrange
+            BinaryTree<Node<int>> tree = new BinaryTree<Node<int>>();
+            Node<int> node = new Node<int>(12);
+            int expected = 12;
+            //Act
+            tree.Add(node);
+            //Assert
+            Assert.AreEqual(expected, tree.root.value);
+        }
+    }
+}
